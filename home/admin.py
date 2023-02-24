5fb78c4a359a4fc37c1feeab3a8ec95f5551c7cf
Karthik Raj <karthikrofficial7@gmail.com>
2023-02-24 14:24:43 +0530

    changed admin site title

--- home/admin.py
+++ home/admin.py
@@ -5,3 +5,8 @@ from .models import PortfolioMaster
 
 # Register your models here.
 admin.site.register(PortfolioMaster)
+
+
+admin.site.site_header = "Thycha Creations Admin"
+admin.site.site_title = "Thycha Creations Admin Page"
+admin.site.index_title = "Welcome to Thycha Creations Page Manager"
